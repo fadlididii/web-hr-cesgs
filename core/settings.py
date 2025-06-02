210f6cdd6a430686fc88da4ba9ff674c8f5faf8a
Fadli Muhammad <fadlimuh3012@gmail.com>
2025-06-02 11:22:44 +0700

    update settings

--- core/settings.py
+++ core/settings.py
@@ -96,20 +96,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'hr_cesgs',
-#         'USER': 'hr_user',
-#         'PASSWORD': 'hr_cesgs',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
-
 load_dotenv()
 
 DATABASES = {
